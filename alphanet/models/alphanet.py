77e54fc3875bc71a649007c92fc5c5f750f68d11
Bangchen Yin <106296634+zmyybc@users.noreply.github.com>
2025-09-28 13:45:45 +0800

    Update alphanet.py

--- alphanet/models/alphanet.py
+++ alphanet/models/alphanet.py
@@ -642,7 +642,7 @@ class AlphaNet(nn.Module):
         else:
             raise ValueError(f"Unexpected shape of s: {s.shape}")
        
-        s = scatter(s, batch, dim=0, reduce=self.readout)+ V_graph
+        s = scatter(s, batch, dim=0, reduce=self.readout).squeeze()+ V_graph
 
         if self.use_sigmoid:
             s = torch.sigmoid((s - 0.5) * 5)
