f5ed52ba4dda6b57ccd5b1a1db6a223f684488ea
Bangchen Yin <106296634+zmyybc@users.noreply.github.com>
2026-01-02 11:27:50 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alphanet',
+    version="0.1.2",
+    packages=find_packages(include=['alphanet', 'alphanet.*']),
+    install_requires=[
+        'pyfiglet',
+        'rich',
+        'ase',
+        'rdkit',
+        'pydantic',
+        'scikit-learn',
+        'pydantic_settings'
+        
+    ],
+    entry_points={
+        "console_scripts": [
+            "alpha-train=alphanet.cli:main",
+            "alpha-eval=alphanet.eval:evaluate",
+            "alpha-conv=alphanet.state_dict:cli",
+            "alpha-freeze=alphanet.freeze:freeze_model"
+        ]
+    },
+    python_requires='>=3.8'
+)
